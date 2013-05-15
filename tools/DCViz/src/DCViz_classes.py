9f834070790440d34e0bf2609d67800990265f3d
jorgehog <jorgen_da_mad@hotmail.com>
2013-05-15 15:33:53 +0200

    added labels to EvsR plot

--- tools/DCViz/src/DCViz_classes.py
+++ tools/DCViz/src/DCViz_classes.py
@@ -442,6 +442,8 @@ class R_vs_E(DCVizPlotter):
         R, E = zip(*sorted(zip(R, E), key=lambda x: x[0]))
         
         self.sfig.plot(R, E, '*')
+        self.sfig.set_xlabel("R")
+        self.sfig.set_ylabel("<V>")
         
         
 
