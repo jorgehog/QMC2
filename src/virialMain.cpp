8f02a08928261e54e9918be14ba96a6e3da98898
jorgehog <jorgen_da_mad@hotmail.com>
2014-02-23 10:06:17 +0100

    upd vir main

--- src/virialMain.cpp
+++ src/virialMain.cpp
@@ -32,9 +32,9 @@ int main()
 
     initMPI(pp, 0, NULL);
 
-    calcVirialPlot(2, 0.01, 1, 2, pp, 0.98831, 0.398664);
-    calcVirialPlot(6, 0.01, 1, 2, pp, 0.9243, 0.5571);
-    calcVirialPlot(12, 0.01, 1, 2, pp, 0.8756, 0.66);
+    calcVirialPlot(2, 0.01, 1, 20, pp, 0.98831, 0.398664);
+    calcVirialPlot(6, 0.01, 1, 20, pp, 0.9243, 0.5571);
+    calcVirialPlot(12, 0.01, 1, 20, pp, 0.8756, 0.66);
     calcVirialPlot(20, 0.01, 1, 20, pp, 0.8361, 0.7332);
     calcVirialPlot(30, 0.01, 1, 20, pp, 0.8085, 0.7944);
     calcVirialPlot(42, 0.01, 1, 20, pp, 0.782778, 0.84400);
